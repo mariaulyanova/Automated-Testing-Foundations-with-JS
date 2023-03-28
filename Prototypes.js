dd88b24375f8230eb15e551ff864316ef4fe5c1e
Maria <116024550+mariaulyanova@users.noreply.github.com>
2023-03-28 20:37:35 +0400

    Update Prototypes.js

--- Prototypes.js
+++ Prototypes.js
@@ -9,3 +9,11 @@ Cat.prototype = Object.create(Animal.prototype);
 const myCat = new Cat();
 console.log(myCat.canMove);
 myCat.move();
+
+
+// const animaal = new Object({
+//   move: 'canMove',
+// })
+// const cat = Object.create(animaal)
+
+// console.log(cat.move)
