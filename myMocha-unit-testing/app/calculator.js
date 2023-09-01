14bc133f3bef217b22f67ae0f69071c942975c4f
Maria Ulyanova <ulyanovamari1@gmail.com>
2023-09-01 20:34:04 +0400

    add some fixes

--- myMocha-unit-testing/app/calculator.js
+++ myMocha-unit-testing/app/calculator.js
@@ -1,9 +1,9 @@
 class Calculator {
   add(...numbers) {
-    if (numbers.length === 1) { 
-      return numbers[0]; 
+    if (numbers.length === 1) {
+      return numbers[0];
     }
-    return numbers.reduce((acc, item) => acc + item);
+    return numbers.reduce((acc, item) => acc + item, 1);
   };
 
   multiply(...numbers) {
